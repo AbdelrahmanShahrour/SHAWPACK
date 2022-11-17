ed99dfcccb6ef057f7835284f39973bfc9345607
Abdelrahman Shahrour <abdalrahman_shahrour@outlook.com>
2022-11-17 23:03:41 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+description = 'Now with us, dealing with Arabic texts has become easy, and in the future it will become easier with us'
+ver = '0.0.1'
+
+
+setup(
+    name='SHAWPACK',
+    version= ver,
+    install_requires=required,
+    tests_require=['pytest'],
+    author="AbdelrahmanShahrour",
+    long_description=description,
+    long_description_content_type='text/x-rst',
+    url='https://github.com/AbdelrahmanShahrour/SHAWPACK',
+    keywords=['NLP', 'text-data', 'AI', 'DS', 'Shahrour', 'arabic-py', 'arabic-nlp'],
+    description="SHAWPACK NLP processing package",
+    packages=find_packages(include=['shawpack'])
+)
