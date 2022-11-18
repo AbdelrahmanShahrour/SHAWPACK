3b3de7ea054abc6a385343c11ef12789c3d214bf
Abdelrahman Shahrour <abdalrahman_shahrour@outlook.com>
2022-11-18 12:05:04 +0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 description = 'Now with us, dealing with Arabic texts has become easy, and in the future it will become easier with us'
-ver = '0.0.2'
+ver = '0.3'
 
 
 setup(
